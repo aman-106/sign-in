446361933e9a60f804470d27f35d491b8387d7ba
aman-106 <amanrags@gmail.com>
2020-04-29 20:54:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useMemo } from "react";
-import { useLoginState, } from './useLoginState';
+import { useLoginState, authUserState } from './useLoginState';
 import Input from './Input';
 import Submit from './Submit';
 import "./styles.css";
@@ -18,25 +18,34 @@ export function Login(props) {
   const btnClsx = validInputs.user && validInputs.password ? "" : "disabled";
   return (
     <div className="form-container sign-in-container">
-      <form action="#">
-        <h1>Sign in</h1>
-        <span className="subheader">Use your HealthifyMe Account</span>
-        <Input
-          error={!validInputs.user}
-          type="email"
-          placeholder="Email"
-          value={user}
-          onChange={handleUser}
-        />
-        <Input
-          error={!validInputs.password}
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePassword}
-        />
-        <Submit btnClsx={btnClsx} handleLogin={handleLogin} />
-      </form>
+      {authState === authUserState.sucess ? (
+        <h2 className="home-page">Home Page</h2>
+      ) : (
+          <form action="#">
+            <h1>Sign in</h1>
+            <span className="subheader">Use your HealthifyMe Account</span>
+            <Input
+              error={!validInputs.user}
+              type="email"
+              placeholder="Email"
+              value={user}
+              onChange={handleUser}
+            />
+            <Input
+              error={!validInputs.password}
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePassword}
+            />
+            <div className={authState === authUserState.loading ? 'loading' : 'non-loading'}>
+              <Submit btnClsx={btnClsx} handleLogin={handleLogin} />
+              <div className="loader"></div>
+            </div>
+          </form>
+        )
+      }
+
     </div>
   );
 }
